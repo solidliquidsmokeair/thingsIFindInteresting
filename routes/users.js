f0d144bfa31b5e78fda0b54e840a2130c64f281a
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-09 16:04:57 +1300

    more routes with express

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,20 @@ router.get('/new', (req, res) => {
     res.send('new users page')
 })
 
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+router
+    .route('/:id')
+.get(('/:id', (req, res) => {
+    res.send(`this is user ${req.params.id}`)
+    }))
+.put(('/:id', (req, res) => {
+    res.send(`this is user ${req.params.id}`)
+    }))
+.delete(('/:id', (req, res) => {
+    res.send(`this is user ${req.params.id}`)
+    }))
+
 module.exports = router
